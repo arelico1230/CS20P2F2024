cb44cf031d69d5bece798b44f5e6fd6755c9b9d9
38343009 <38343009@L815WD220002.edu.cbe.ab.ca>
2024-12-19 10:23:09 -0700

    a

--- GettingStarted/src/Thermostats.java
+++ GettingStarted/src/Thermostats.java
@@ -3,6 +3,9 @@ import com.phidget22.*;
 
 public class Thermostats {
 	
+	//set temperature at 21°C
+	    static int temp = 21;
+	
 	public static void main(String[] args) throws Exception {
 	    // Create | Create objects for sensors, buttons, and LEDs
 	    HumiditySensor humiditySensor = new HumiditySensor();
@@ -12,8 +15,7 @@ public class Thermostats {
 	    DigitalOutput redLED = new DigitalOutput();
 	    DigitalOutput greenLED = new DigitalOutput();
 	    
-	  //set temperature at 21°C
-	    static double temp = 21.0;
+	 
 
 	  //Address | Address your four objects which lets your program know where to find them.
 	    redButton.setHubPort(0);
@@ -25,47 +27,31 @@ public class Thermostats {
         greenLED.setHubPort(1);
         greenLED.setIsHubPortDevice(true);
         
-        
-        
-        
-        
-        redButton.addStateChangeListener(new DigitalInputStateChangeListener() {
-	        public void onStateChange (DigitalInputStateChangeEvent y) {
-	        	if (y.getState() == true) {
-	        		temp--;
-	        	System.out.println("\ntemperature decreased by 1 °C" );
-	        	
-	        	
-	        }
-	        }
-	 });
-        
-        
-        
-        
-        
-        redButton.addStateChangeListener(new DigitalInputStateChangeListener() {
-	        public void onStateChange (DigitalInputStateChangeEvent y) {
-	        	if (y.getState() == true) {
-	        		temp--;
-	        	System.out.println("\ntemperature decreased by 1 °C" );
-	        	
-	        	
-	        }
-	        }
-	 });
-        
-        
-        
-        
-        
       //Open | Connect your program to your physical devices.
-	    humiditySensor.open(1000);
 	    temperatureSensor.open(1000);
 	    redButton.open(1000);
         redLED.open(1000);
         greenButton.open(1000);
         greenLED.open(1000);
+        
+        
+        redButton.addStateChangeListener(new DigitalInputStateChangeListener() {
+	        public void onStateChange (DigitalInputStateChangeEvent y) {
+	        	if (y.getState() == true) {
+	        		temp--;
+	        	System.out.println("Temperature decreased by 1 °C" );
+	        }
+	        }
+	 });
+        
+        greenButton.addStateChangeListener(new DigitalInputStateChangeListener() {
+	        public void onStateChange (DigitalInputStateChangeEvent y) {
+	        	if (y.getState() == true) {
+	        		temp++;
+	        	System.out.println("Temperature increased by 1 °C" );
+	        }
+	        }
+	 });
         
         
         
@@ -74,15 +60,16 @@ public class Thermostats {
         	
              System.out.println("Current temperature: " + temperatureSensor.getTemperature() + " °C");
              System.out.println("Set temperature: " + temp + " °C");
-             Thread.sleep(1000);
+             Thread.sleep(10000);
         	
         	if (temperatureSensor.getTemperature() > (temp + 2) && temperatureSensor.getTemperature() < (temp - 2)) {
+        		
         	greenLED.setState(true);
         	Thread.sleep(100);
         	greenLED.setState(false);
         	Thread.sleep(100);
-        	continue;
-        	} else {
+        	continue;}
+        	else {
         		redLED.setState(true);
         		Thread.sleep(100);
         		redLED.setState(false);
